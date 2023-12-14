0201822421de86bcccd90ac1a3c8fddfd24c0e86
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-12-14 14:14:40 +0000

    initial commit

---
+++ Gymgraze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 14/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gymgraze/GymgrazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymgrazeApp.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GymgrazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
