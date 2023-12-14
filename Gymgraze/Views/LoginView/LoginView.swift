988369634f4de7dd708e1337349bdb0a9f342355
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2023-12-14 14:17:36 +0000

    Add basic structure

---
+++ Gymgraze/Views/LoginView/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 14/12/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Gymgraze/Views/LoginView/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 14/12/2023.
+//
+
+import Foundation
