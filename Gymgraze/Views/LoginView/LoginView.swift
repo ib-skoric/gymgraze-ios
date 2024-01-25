6fb6e63f1b4fcabf46f61ebd6bac46f33611bbe3
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-25 21:43:17 +0000

    Add comments

--- Gymgraze/Views/LoginView/LoginView.swift
+++ Gymgraze/Views/LoginView/LoginView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct LoginView: View {
     
+    // add two state variables to store the email and password
     @State var email: String = ""
     @State var password: String = ""
     
     var body: some View {
         VStack {
+            // add the logo
             Image("logo").resizable().frame(width: 150, height: 150)
+            // add in two custom input fields
             LoginInput(data: $email, title: "Email").accessibilityLabel("Email input field")
             LoginInput(data: $password, title: "Password").accessibilityLabel("Password input field")
+            // add in the login button
             Button(action: {
                 print("Login button pressed")
             }, label: {
@@ -24,8 +28,6 @@ struct LoginView: View {
             }).buttonStyle(LoginButton())
                 .padding()
         }
-        
-        
     }
 }
 
