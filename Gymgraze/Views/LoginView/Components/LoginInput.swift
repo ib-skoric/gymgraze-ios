f48e2fc2d100f3a99e862aa42b69d5f90349134d
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-01-25 20:47:39 +0000

    Create login input component

---
+++ Gymgraze/Views/LoginView/Components/LoginInput.swift
@@ -0,0 +1,18 @@
+//
+//  LoginInput.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 25/01/2024.
+//
+
+import SwiftUI
+
+struct LoginInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginInput()
+}
