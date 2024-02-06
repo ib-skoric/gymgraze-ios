c8c0e2f1e3d1c669e507dffdc8514c9f1ef0b496
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-06 08:37:42 +0000

    Save token to keychain

--- Gymgraze/ViewModels/LoginViewModel.swift
+++ Gymgraze/ViewModels/LoginViewModel.swift
@@ -17,7 +17,14 @@ class LoginViewModel: ObservableObject {
         WebService().authenticate(username: username, password: password) { (result) in
             switch result {
             case .success(let token):
-                print(token)
+                let tokenData = token.data(using: .utf8)!
+                let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                           kSecAttrAccount as String: "token",
+                                           kSecValueData as String: tokenData]
+                let status = SecItemAdd(query as CFDictionary, nil)
+                if status == errSecSuccess {
+                    print("Token saved successfully")
+                }
             case .failure(let error):
                 // if the login failed, print the error
                 print(error)
