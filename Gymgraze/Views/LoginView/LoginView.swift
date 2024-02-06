5e912650bf902bc84da6cc04abe262b2f952b5eb
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-06 20:53:15 +0000

    Add transition to a different screen after log in

--- Gymgraze/Views/LoginView/LoginView.swift
+++ Gymgraze/Views/LoginView/LoginView.swift
@@ -13,33 +13,33 @@ struct LoginView: View {
     @State var email: String = ""
     @State var password: String = ""
     
-    var isSignInButtonDisabled: Bool {
-        return email.isEmpty || password.isEmpty
-    }
-    
     @StateObject private var loginVM = LoginViewModel()
     
     var body: some View {
-        
-        VStack {
-            // add the logo
-            Image("logo").resizable().frame(width: 150, height: 150)
-            // add in two custom input fields
-            LoginInput(data: $loginVM.username, title: "Email").accessibilityLabel("Email input field")
-            LoginInput(data: $loginVM.password, title: "Password").accessibilityLabel("Password input field")
-            // add in the login button
-            Button(action: {
-                print("Login button pressed")
-                loginVM.authenticate()
-            }, label: {
-                Text("Login")
-            }).buttonStyle(LoginButton())
-                .padding()
-                .accessibilityLabel("Login button")
-                .disabled(isSignInButtonDisabled)
-        }
-        .alert(isPresented: $loginVM.authenticationError) {
-            Alert(title: Text("Authentication Error"), message: Text("Invalid username or password"), dismissButton: .default(Text("OK")))
+        NavigationStack {
+            if loginVM.authenticated {
+                ContentView()
+            } else {
+                VStack {
+                    // add the logo
+                    Image("logo").resizable().frame(width: 150, height: 150)
+                    // add in two custom input fields
+                    LoginInput(data: $loginVM.username, title: "Email").accessibilityLabel("Email input field")
+                    LoginInput(data: $loginVM.password, title: "Password").accessibilityLabel("Password input field")
+                    // add in the login button
+                    Button(action: {
+                        print("Login button pressed")
+                        loginVM.authenticate()
+                    }, label: {
+                        Text("Login")
+                    }).buttonStyle(LoginButton())
+                        .padding()
+                        .accessibilityLabel("Login button")
+                }
+                .alert(isPresented: $loginVM.authenticationError) {
+                    Alert(title: Text("Authentication Error"), message: Text("Invalid username or password"), dismissButton: .default(Text("OK")))
+                }
+            }
         }
     }
 }
