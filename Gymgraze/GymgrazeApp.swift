3beec5cdeea9de61bd1440ad2bd060f22336f9ea
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-06 21:08:10 +0000

    Add in conditional to display different main screen based on if token is set or not

--- Gymgraze/GymgrazeApp.swift
+++ Gymgraze/GymgrazeApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct GymgrazeApp: App {
+    @StateObject var loginVM = LoginViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            if getToken() != nil {
+                ContentView()
+            } else {
+                LoginView()
+            }
         }
     }
+    
+    func getToken() -> String? {
+        let query: [String: Any] = [kSecClass as String: kSecClassGenericPassword,
+                                    kSecAttrAccount as String: "token",
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecReturnData as String: kCFBooleanTrue!]
+        
+        var dataTypeRef: AnyObject?
+        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        
+        if status == noErr {
+            if let data = dataTypeRef as? Data {
+                return String(data: data, encoding: .utf8)
+            }
+        }
+        return nil
+    }
 }
+
