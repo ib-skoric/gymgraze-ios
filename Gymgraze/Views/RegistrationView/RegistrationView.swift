6139d958329c87a12f8381fd71d535661e885cbe
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-08 21:38:28 +0000

    Change button based on step type

--- Gymgraze/Views/RegistrationView/RegistrationView.swift
+++ Gymgraze/Views/RegistrationView/RegistrationView.swift
@@ -35,54 +35,57 @@ struct RegistrationView: View {
             VStack {
                 Text("What's a good email to contact you on?")
                     .multilineTextAlignment(.center)
-                .font(.subheadline)
+                    .font(.subheadline)
                 
                 InputField(data: $email, title: "Email").accessibilityLabel("Email input field")
-
+                
             }.transition(.push(from: .trailing))
-
-    
-            } else if step == 1 {
-                
-                VStack {
-                    Text("Give us a password to keep your account safe and sound!")
-                        .multilineTextAlignment(.center)
+            
+            
+        } else if step == 1 {
+            
+            VStack {
+                Text("Give us a password to keep your account safe and sound!")
+                    .multilineTextAlignment(.center)
                     .font(.subheadline)
-                    
-                    InputField(data: $password, title: "Password").accessibilityLabel("Password input field")
-                }.transition(.push(from: .trailing))
-            } else if step == 2 {
                 
-                VStack {
-                    Text("What should we call you?")
-                        .multilineTextAlignment(.center)
+                InputField(data: $password, title: "Password").accessibilityLabel("Password input field")
+            }.transition(.push(from: .trailing))
+        } else if step == 2 {
+            
+            VStack {
+                Text("What should we call you?")
+                    .multilineTextAlignment(.center)
                     .font(.subheadline)
-                    
-                    InputField(data: $name, title: "Name").accessibilityLabel("Name input field")
-                }
-                    .transition(.push(from: .trailing))
+                
+                InputField(data: $name, title: "Name").accessibilityLabel("Name input field")
             }
+            .transition(.push(from: .trailing))
+        }
         
         
         Spacer()
         
-        Button(action: {
-            print("Next button pressed for step \(step)")
-            withAnimation {
-                // check if we are at the last step
-                if step == 2 {
-                    // if so, submit the form
-                    print("Submitting form")
-                } else {
+        if step != 2 {
+            Button(action: {
+                print("Next button pressed for step \(step)")
+                withAnimation {
                     step += 1
                 }
-            }
-        }, label: {
-            Text("Next")
-        }).buttonStyle(CTAButton())
-            .padding()
-            .accessibilityLabel("Next step button")
-        
+            }, label: {
+                Text("Next")
+            }).buttonStyle(CTAButton())
+                .padding()
+                .accessibilityLabel("Next step button")
+        } else {
+            Button(action: {
+                print("Sign up button pressed")
+            }, label: {
+                Text("Sign up!")
+            }).buttonStyle(CTAButton())
+                .padding()
+                .accessibilityLabel("Sign up button")
+        }
     }
 }
 
