07699683a5668b02441786ebb2cfac471dbddabb
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-02-10 14:17:26 +0000

    Optimise the registration screen

--- Gymgraze/Views/RegistrationView/RegistrationView.swift
+++ Gymgraze/Views/RegistrationView/RegistrationView.swift
@@ -14,7 +14,26 @@ struct RegistrationView: View {
     @State var name: String = ""
     @State var step: Int = 0
     @State var age: String = ""
-    
+    @State var weight: String = ""
+    @State var height: String = ""
+
+    struct Step {
+        let question: String
+        let placeholder: String
+        let binding: Binding<String>
+    }
+
+    var steps: [Step] {
+        [
+            Step(question: "What's a good email to contact you on?", placeholder: "Email", binding: $email),
+            Step(question: "Give us a password to keep your account safe and sound!", placeholder: "Password", binding: $password),
+            Step(question: "What should we call you?", placeholder: "Name", binding: $name),
+            Step(question: "What is your age?", placeholder: "Age", binding: $age),
+            Step(question: "What is your current weight?", placeholder: "Weight", binding: $weight),
+            Step(question: "What is your current height?", placeholder: "Height", binding: $height)
+        ]
+    }
+
     var body: some View {
         VStack {
             Text("Join us!")
@@ -31,77 +50,28 @@ struct RegistrationView: View {
         
         Spacer()
         
-        if step == 0 {
-            
+        if step < steps.count {
             VStack {
-                Text("What's a good email to contact you on?")
+                Text(steps[step].question)
                     .multilineTextAlignment(.center)
                     .font(.subheadline)
                 
-                InputField(data: $email, title: "Email").accessibilityLabel("Email input field")
-                
-            }.transition(.push(from: .trailing))
-            
-            
-        } else if step == 1 {
-            
-            VStack {
-                Text("Give us a password to keep your account safe and sound!")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                
-                InputField(data: $password, title: "Password").accessibilityLabel("Password input field")
-            }.transition(.push(from: .trailing))
-        } else if step == 2 {
-            
-            VStack {
-                Text("What should we call you?")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                
-                InputField(data: $name, title: "Name").accessibilityLabel("Name input field")
-            }
-            .transition(.push(from: .trailing))
-        } else if step == 3 {
-            VStack {
-                Text("What is your age?")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                
-                InputField(data: $age, title: "Age").accessibilityLabel("Age input field")
-            }
-            .transition(.push(from: .trailing))
-        } else if step == 4 {
-            VStack {
-                Text("What is your current weight?")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                
-                InputField(data: $age, title: "Weight").accessibilityLabel("Weight input field")
-            }
-            .transition(.push(from: .trailing))
-        } else if step == 5 {
-            VStack {
-                Text("What is your current height?")
-                    .multilineTextAlignment(.center)
-                    .font(.subheadline)
-                
-                InputField(data: $age, title: "Height").accessibilityLabel("Height input field")
+                InputField(data: steps[step].binding, title: steps[step].placeholder)
+                    .accessibilityLabel("\(steps[step].placeholder) input field")
             }
             .transition(.push(from: .trailing))
         } else {
             VStack {
-                Text("Thank you for signing up!")
+                Text("That's all!\n\n Ready?\n Hit the button below and let's go! 🚀")
                     .multilineTextAlignment(.center)
                     .font(.headline)
             }
             .transition(.push(from: .trailing))
         }
         
-        
         Spacer()
         
-        if step <= 4  {
+        if step < steps.count {
             Button(action: {
                 print("Next button pressed for step \(step)")
                 withAnimation {
@@ -112,20 +82,21 @@ struct RegistrationView: View {
             }).buttonStyle(CTAButton())
                 .padding()
                 .accessibilityLabel("Next step button")
-        } else if step == 5 {
+        } else {
             Button(action: {
                 print("Sign up button pressed")
                 withAnimation {
                     step += 1
                 }
             }, label: {
-                Text("Sign up!")
+                Text("Sign up")
             }).buttonStyle(CTAButton())
                 .padding()
                 .accessibilityLabel("Sign up button")
         }
     }
 }
+
 
 #Preview {
     RegistrationView()
