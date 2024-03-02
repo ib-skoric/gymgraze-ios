8259237a7db69af65cf3cbddce85a57d09682a5b
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-02 15:38:57 +0000

    Add barebones layout for resetting password

---
+++ Gymgraze/Views/PasswordResetView.swift
@@ -0,0 +1,51 @@
+//
+//  PasswordResetView.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 02/03/2024.
+//
+
+import SwiftUI
+
+struct PasswordResetView: View {
+    
+    @State var passwordResetEmail: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Reset your password")
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("It happens to everyone, no worries!")
+                    .multilineTextAlignment(.center)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .padding(.top)
+            
+            Spacer()
+            
+            Text("What's the email your account was registered with?")
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            InputField(data: $passwordResetEmail, title: "Your email")
+            
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Text("Send password reset code")
+            }).buttonStyle(CTAButton())
+                .padding()
+                .accessibilityLabel("Send password reset code button")
+        }
+    }
+    }
+
+#Preview {
+    PasswordResetView()
+}
