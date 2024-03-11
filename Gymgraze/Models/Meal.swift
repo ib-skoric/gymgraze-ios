671ad3cdf11ce67c85cc766f8e48b693bb53a905
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-11 08:32:28 +0000

    Add date decoder to Meal

--- Gymgraze/Models/Meal.swift
+++ Gymgraze/Models/Meal.swift
@@ -11,13 +11,26 @@ import Foundation
 struct Meal: Codable, Hashable {
     var id: Int
     var name: String
-    var createdAt: String
-    var updatedAt: String
+    var createdAt: Date
+    var updatedAt: Date
     
     enum CodingKeys: String, CodingKey {
         case id
         case name
         case createdAt = "created_at"
         case updatedAt = "updated_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        
+        // Decode the ISO8601 date string into a Swift Date
+        let isoDateCreatedAt = try container.decode(String.self, forKey: .createdAt)
+        let isoDateUpdatedAt = try container.decode(String.self, forKey: .updatedAt)
+        let dateFormatter = ISO8601DateFormatter()
+        createdAt = dateFormatter.date(from: isoDateCreatedAt)!
+        updatedAt = dateFormatter.date(from: isoDateUpdatedAt)!
     }
 }
