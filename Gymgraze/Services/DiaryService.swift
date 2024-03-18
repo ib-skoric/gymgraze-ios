5ff948965bee8afc09dcd6a789b82476c644f264
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-18 21:23:08 +0000

    Add method to update amount for a food item

--- Gymgraze/Services/DiaryService.swift
+++ Gymgraze/Services/DiaryService.swift
@@ -58,5 +58,43 @@ class DiaryService {
     func fetchFoodItem(foodId: Int, completion: @escaping (Result<Food, APIError>) -> Void) {
         fetch(urlString: "http://localhost:3000/foods/\(foodId)", completion: completion)
     }
+    
+    func updateFoodAmount(foodId: Int, amount: Int, completion: @escaping (Result<Food, APIError>) -> Void) {
+        let token: String? = getToken()
+        
+        guard let url = URL(string: "http://localhost:3000/food/\(foodId)") else {
+            completion(.failure(APIError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-type")
+        request.addValue("Bearer \(token ?? "not set")", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                completion(.failure(APIError.serverDown))
+                return
+            }
+            
+            if let httpResonse = response as? HTTPURLResponse {
+                switch httpResonse.statusCode {
+                case 200:
+                    guard let diaryResponse = try? JSONDecoder().decode(Food.self, from: data) else {
+                        completion(.failure(APIError.invalidDataReturnedFromAPI))
+                        return
+                    }
+                    completion(.success(diaryResponse))
+                case 401:
+                    completion(.failure(APIError.invalidCredentials))
+                case 404:
+                    completion(.failure(APIError.entryNotFound))
+                default:
+                    completion(.failure(APIError.custom(errorMessage: "Status code: \(httpResonse.statusCode)")))
+                }
+            }
+        }.resume()
+    }
 }
 
