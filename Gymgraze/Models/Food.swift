23b6f4ebdb5b20c17bfcd636820667f70105ea12
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-18 10:59:19 +0000

    Fix up nested struct

--- Gymgraze/Models/Food.swift
+++ Gymgraze/Models/Food.swift
@@ -21,3 +21,31 @@ struct Food: Codable, Identifiable {
         case meal
     }
 }
+
+struct FoodItem: Codable, Identifiable {
+    var id: String?
+    var barcode: String
+    var nutriments: Nutriments
+    
+    struct Nutriments: Codable {
+        var kcal100g: Int
+        var carbs100g: Double
+        var protein100g: Double
+        var fat100g: Double
+        var salt100g: Double
+        var sugar100g: Double
+        var fiber100g: Double
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case barcode = "code"
+        case nutriments
+    }
+    
+    init(id: String?, barcode: String, nutriments: Nutriments) {
+        self.id = barcode
+        self.barcode = barcode
+        self.nutriments = nutriments
+    }
+}
