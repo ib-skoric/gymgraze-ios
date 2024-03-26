ec9d1940e1f3ca96f92ef21c1a040a20d166f93c
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-26 19:56:28 +0000

    Clean up exercise model

--- Gymgraze/Models/Exercise.swift
+++ Gymgraze/Models/Exercise.swift
@@ -7,17 +7,13 @@
 
 import Foundation
 
-class Exercise: Codable, Identifiable, Equatable, ObservableObject {
-    
-    
+class Exercise: Codable, Identifiable, ObservableObject {
     let id: Int
-    let name: String
-    let exerciseType: String
+    let exerciseType: Int
     @Published var exerciseSets: [ExerciseSet]?
     
     enum CodingKeys: String, CodingKey {
         case id
-        case name
         case exerciseType = "exercise_type"
         case exerciseSets = "exercise_sets"
     }
@@ -46,23 +42,20 @@ class Exercise: Codable, Identifiable, Equatable, ObservableObject {
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(Int.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        exerciseType = try container.decode(String.self, forKey: .exerciseType)
+        exerciseType = try container.decode(Int.self, forKey: .exerciseType)
         exerciseSets = try container.decodeIfPresent([ExerciseSet].self, forKey: .exerciseSets)
     }
     
     func encode(to encoder: Encoder) throws {
             var container = encoder.container(keyedBy: CodingKeys.self)
             try container.encode(id, forKey: .id)
-            try container.encode(name, forKey: .name)
             try container.encode(exerciseType, forKey: .exerciseType)
             try container.encode(exerciseSets, forKey: .exerciseSets)
         }
     
     init() {
         id = 0
-        name = ""
-        exerciseType = ""
+        exerciseType = 0
         exerciseSets = []
     }
 }
