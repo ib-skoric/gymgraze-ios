f5022c8ab2c0658e8965567bb0a07bc3d6563f4b
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-03-31 13:43:57 +0100

    Add duration to workout

--- Gymgraze/Models/Workout.swift
+++ Gymgraze/Models/Workout.swift
@@ -11,12 +11,14 @@ struct Workout: Codable, Identifiable {
     let id: Int
     let date: String
     let workoutDiaryEntryId: Int
+    let duration: Int
     let exercises: [Exercise]
     
     enum CodingKeys: String, CodingKey {
         case id
         case date
         case workoutDiaryEntryId = "workout_diary_entry_id"
+        case duration
         case exercises
     }
     
@@ -24,13 +26,15 @@ struct Workout: Codable, Identifiable {
         id = 0
         date = ""
         workoutDiaryEntryId = 0
+        duration = 0
         exercises = []
     }
     
-    init(id: Int, date: String, workoutDiaryEntryId: Int, exercises: [Exercise]) {
+    init(id: Int, date: String, workoutDiaryEntryId: Int, duration: Int, exercises: [Exercise]) {
         self.id = id
         self.date = date
         self.workoutDiaryEntryId = workoutDiaryEntryId
+        self.duration = duration
         self.exercises = exercises
     }
 }
