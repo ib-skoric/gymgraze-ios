3745d6ecf330ef7bfb7a91b41e525fbce581674f
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-06 13:36:34 +0100

    Try to fix some animations

--- Gymgraze/Views/Diary/DiaryView.swift
+++ Gymgraze/Views/Diary/DiaryView.swift
@@ -8,6 +8,7 @@ struct DiaryView: View {
     @State private var selectedWorkout: Workout?
     @State private var isAddFoodViewPresented: Bool = false
     @State private var isAddWorkoutViewPresented: Bool = false
+    @State private var isWorkoutDetailsActive: Bool = false
     @State var selectedDate: Date?
     
     var foodsByMeal: [Int: [Food]] {
@@ -19,105 +20,83 @@ struct DiaryView: View {
             diaryVM.selectedDate = selectedDate!
         }
     }
-
+    
     var body: some View {
-        ZStack {
-            VStack {
-                NavigationStack {
-                    VStack {
-                        HStack {
-                            Heading(text: "📒 Diary")
-                            Spacer()
-                            DatePicker(selection: $diaryVM.selectedDate, displayedComponents: .date) {
-                                EmptyView()
-                            }
-                            .onChange(of: diaryVM.selectedDate) { newValue, oldValue in
-                                DispatchQueue.main.async {
-                                    if newValue != oldValue {
-                                        selectedDate = newValue
-                                        diaryVM.fetchFoodDiary()
-                                        diaryVM.fetchWorkoutDiary()
+        VStack {
+            NavigationStack {
+                VStack {
+                    viewHeading
+                }
+                .onAppear {
+                    DispatchQueue.main.async {
+                        diaryVM.refresh()
+                    }
+                }
+                VStack {
+                    if diaryVM.isLoading {
+                        Spacer()
+                        VStack {
+                            ProgressView()
+                        }
+                        Spacer()
+                    } else if diaryVM.diaryFoods.isEmpty && diaryVM.diaryWokrouts.isEmpty {
+                        Spacer()
+                        noEtriesView
+                        Spacer()
+                    } else {
+                        List {
+                            if !diaryVM.diaryFoods.isEmpty {
+                                ForEach(foodsByMeal.keys.sorted(), id: \.self) { mealId in
+                                    Section() {
+                                        ForEach(foodsByMeal[mealId]!, id: \.id) { food in
+                                            FoodDiaryRow(food: food, nutritionalInfo: food.nutritionalInfo)
+                                                .onTapGesture {
+                                                    selectedFood = food
+                                                }
+                                        }
+                                        .onDelete(perform: { indexSet in
+                                            deleteFood(mealId: mealId)
+                                        })
+                                    } header: {
+                                        HStack {
+                                            VStack {
+                                                Text(diaryVM.diaryFoods.first(where: { $0.meal.id ==  mealId })?.meal.name ?? "")
+                                            }
+                                            
+                                            Spacer()
+                                            
+                                            HStack {
+                                                Text("**Kcal**: \(foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.kcal })")
+                                                Text("**C**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.carbs }))")
+                                                Text("**P**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.protein }))")
+                                                Text("**F**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.fat }))")
+                                            }
+                                        }
                                     }
+                                }
+                                .sheet(item: $selectedFood) { food in
+                                    FoodDetailView(food: food)
+                                        .environmentObject(diaryVM)
                                 }
                             }
                             
-                            AddToDropdown(type: "menu", date: $diaryVM.selectedDate)
-                        }
-                    }
-                    .onAppear {
-                        DispatchQueue.main.async {
-                            diaryVM.refresh()
-                        }
-                    }
-                    VStack {
-                        if diaryVM.isLoading {
-                            Spacer()
-                            VStack {
-                                ProgressView()
-                            }
-                            Spacer()
-                        } else if diaryVM.diaryFoods.isEmpty && diaryVM.diaryWokrouts.isEmpty {
-                            Spacer()
-                            VStack {
-                                Text("No entries for this date.")
-                                    .font(.title)
-                                    .foregroundColor(.gray)
-                                AddToDropdown(type: "button", date: $diaryVM.selectedDate)
-                            }
-                            .padding()
-                            Spacer()
-                        } else {
-                            List {
-                                if !diaryVM.diaryFoods.isEmpty {
-                                    ForEach(foodsByMeal.keys.sorted(), id: \.self) { mealId in
-                                        Section() {
-                                            ForEach(foodsByMeal[mealId]!, id: \.id) { food in
-                                                FoodDiaryRow(food: food, nutritionalInfo: food.nutritionalInfo)
-                                                    .onTapGesture {
-                                                        selectedFood = food
+                            if !diaryVM.diaryWokrouts.isEmpty {
+                                Section("Workout Diary") {
+                                    ForEach(diaryVM.diaryWokrouts, id: \.id) { workout in
+                                        WorkoutDiaryRow(workout: workout)
+                                            .onTapGesture {
+                                                if self.selectedWorkout == nil {
+                                                    DispatchQueue.main.async {
+                                                        self.selectedWorkout = workout
                                                     }
-                                            }
-                                            .onDelete(perform: { indexSet in
-                                                deleteFood(mealId: mealId)
-                                            })
-                                        } header: {
-                                            HStack {
-                                                VStack {
-                                                    Text(diaryVM.diaryFoods.first(where: { $0.meal.id ==  mealId })?.meal.name ?? "")
-                                                }
-                                                
-                                                Spacer()
-                                                
-                                                HStack {
-                                                    Text("**Kcal**: \(foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.kcal })")
-                                                    Text("**C**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.carbs }))")
-                                                    Text("**P**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.protein }))")
-                                                    Text("**F**: \(String(format: "%.1f", foodsByMeal[mealId]!.reduce(0) { $0 + $1.totalNutrition.fat }))")
                                                 }
                                             }
-                                        }
-                                    }
-                                    .sheet(item: $selectedFood) { food in
-                                        FoodDetailView(food: food)
-                                            .environmentObject(diaryVM)
                                     }
                                 }
-                                
-                                if !diaryVM.diaryWokrouts.isEmpty {
-                                    Section("Workout Diary") {
-                                        ForEach(diaryVM.diaryWokrouts, id: \.id) { workout in
-                                            WorkoutDiaryRow(workout: workout)
-                                                .onTapGesture {
-                                                    selectedWorkout = workout
-                                                }
-                                        }
-                                    }
-                                    .foregroundColor(.orange)
-                                    .sheet(item: $selectedWorkout) { workout in
-                                        WorkoutDetailView(workout: workout)
-                                    }
+                                .foregroundColor(.orange)
+                                .sheet(item: $selectedWorkout) { workout in
+                                    WorkoutDetailView(workout: workout)
                                 }
-                                
                             }
                         }
                     }
@@ -125,6 +104,39 @@ struct DiaryView: View {
             }
         }
     }
+    
+    private var viewHeading: some View {
+        HStack {
+            Heading(text: "📒 Diary")
+            Spacer()
+            DatePicker(selection: $diaryVM.selectedDate, displayedComponents: .date) {
+                EmptyView()
+            }
+            .onChange(of: diaryVM.selectedDate) { newValue, oldValue in
+                DispatchQueue.main.async {
+                    if newValue != oldValue {
+                        selectedDate = newValue
+                        diaryVM.fetchFoodDiary()
+                        diaryVM.fetchWorkoutDiary()
+                    }
+                }
+            }
+            
+            AddToDropdown(type: "menu", date: $diaryVM.selectedDate)
+        }
+    }
+    
+    
+    private var noEtriesView: some View {
+        VStack {
+            Text("No entries for this date.")
+                .font(.title)
+                .foregroundColor(.gray)
+            AddToDropdown(type: "button", date: $diaryVM.selectedDate)
+        }
+        .padding()
+    }
+    
     
     func deleteFood(mealId: Int) {
         let foodToDelete = foodsByMeal[mealId]![0].id
