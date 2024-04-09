8a45669be4818af41e6b6f8e8c1084031c4596b7
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-09 21:39:31 +0100

    Add components to edit meals

--- Gymgraze/Views/Profile/EditMealsView.swift
+++ Gymgraze/Views/Profile/EditMealsView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct EditMealsview: View {
+    
+    @EnvironmentObject var userVM: UserViewModel
+    
     var body: some View {
         NavigationView {
             VStack {
                 Heading(text: "Edit meals")
+                
+                List(userVM.user?.meals ?? []) { meal in
+                    HStack {
+                        TextField(meal.name, text: .constant(meal.name))
+                    }
+                }
+                
                 Spacer()
+                
+                Button(action: {
+                    // TODO: Add action code here
+                }, label: {
+                    Text("Save changes")
+                })
+                .buttonStyle(CTAButton())
+                .padding()
             }
         }
     }
