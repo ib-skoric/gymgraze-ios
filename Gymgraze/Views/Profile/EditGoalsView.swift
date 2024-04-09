1b0a819bc98a1d9174175c41a0d41cd866bf4aba
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-09 21:32:36 +0100

    Add components to edit goals

--- Gymgraze/Views/Profile/EditGoalsView.swift
+++ Gymgraze/Views/Profile/EditGoalsView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct EditGoalsView: View {
+    
+    @EnvironmentObject var userVM: UserViewModel
+    
     var body: some View {
         NavigationView {
             VStack {
                 Heading(text: "Edit goals")
+                
+                if let stepsCount = userVM.user?.goal?.steps {
+                    InputField(data: .constant(String(stepsCount)), title: "👟 Target step count per day")
+                }
+                
+                if let exerciseMinutes = userVM.user?.goal?.exercise {
+                    InputField(data: .constant(String(exerciseMinutes)), title: "🏋️‍♂️ Target exercise daily (in minutes)")
+                }
+                
+                if let calories = userVM.user?.goal?.kcal {
+                    InputField(data: .constant(String(calories)), title: "🍏 Calories to consume per day (kcal)")
+                }
+                
                 Spacer()
+                
+                Button(action: {
+                    // TODO: Add action code here
+                }, label: {
+                    Text("Save changes")
+                })
+                .buttonStyle(CTAButton())
+                .padding()
             }
         }
     }
