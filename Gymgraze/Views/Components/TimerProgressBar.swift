c73c54b100f7f225a8ed01fc76d94f1c3438e213
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-04-22 21:19:57 +0100

    Fix up the UI a bit more and add progress bar

---
+++ Gymgraze/Views/Components/TimerProgressBar.swift
@@ -0,0 +1,24 @@
+//
+//  TimerProgressBar.swift
+//  Gymgraze
+//
+//  Created by Ivan Branimir Skoric on 22/04/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width * CGFloat(configuration.fractionCompleted ?? 0)
+            ZStack(alignment: .leading) {
+                Capsule()
+                    .foregroundColor(Color.gray.opacity(0.2))
+                Capsule()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.purple, .orange]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: width)
+            }
+        }
+    }
+}
