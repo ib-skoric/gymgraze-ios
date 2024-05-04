9dbb7275d363c68ed812915702797e7ed79f215b
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-04 21:02:22 +0100

    Genie AI view

--- Gymgraze/ViewModels/GenieAIViewModel.swift
+++ Gymgraze/ViewModels/GenieAIViewModel.swift
@@ -9,32 +9,25 @@ import Foundation
 import SwiftUI
 
 class GenieAIViewModel: ObservableObject {
+    /// Published properties used by different views to update UI
     @Published var totalDayKcal: Double = 0
     @Published var isLoading: Bool = false
     @Published var latestRecipe: String = ""
     @Published var fetchComplete: Bool = false
     
-    func fetchFoodSummary() {
-        DiaryService().fetchFoodSummary { result in
-            switch result {
-            case .success(let foodSummary):
-                self.totalDayKcal = foodSummary.kcal
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
+    /// method for getting the recipe from OpenAI using the provided method and macro values
     func getRecipe(method: String, kcal: String, protein: String, carbs: String, fat: String) {
         withAnimation {
             self.isLoading = true
         }
         
+        // convert the macro values to Double
         let kcalDouble = Double(kcal) ?? 0
         let proteinDouble = Double(protein) ?? 0
         let carbsDouble = Double(carbs) ?? 0
         let fatDouble = Double(fat) ?? 0
         
+        // call the API service to get the recipe
         OpenAIService().getOpenAIResponse(method: method, kcal: kcalDouble, protein: proteinDouble, carbs: carbsDouble, fat: fatDouble) { result in
             switch result {
             case .success(let recipe):
