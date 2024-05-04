1966cbf9c4ed3618c02643e11ae20cdd4c234b7f
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-04 20:53:10 +0100

    Add food DVM

--- Gymgraze/ViewModels/AddToFoodDiaryViewModel.swift
+++ Gymgraze/ViewModels/AddToFoodDiaryViewModel.swift
@@ -9,12 +9,16 @@ import Foundation
 
 class AddToFoodDiaryViewModel: ObservableObject {
     
+    /// Published properties used by different views to update UI
     @Published var foodItems: [FoodItem.Product] = []
     @Published var isLoading: Bool = false
     @Published var searchCompleted: Bool = false
     
+    /// Method for searching for food items
     func searchForFood(searchTerm: String) {
         self.isLoading = true
+        
+        // call the API service to search for food items
         OpenFoodFactsService().searchForFood(searchTerm: searchTerm) { result in
             switch result {
             case .success(let foodItems):
