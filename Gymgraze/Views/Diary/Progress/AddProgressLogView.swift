71f5871d16a704e0bfe5ddc97003ae36cd6e75a3
Ivan Branimir Skoric <58559012+ib-skoric@users.noreply.github.com>
2024-05-26 15:40:23 +0100

    Add same for progress view

--- Gymgraze/Views/Diary/Progress/AddProgressLogView.swift
+++ Gymgraze/Views/Diary/Progress/AddProgressLogView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AddProgressLogView: View {
+    
+    // state and env variables to handle view updates
     @Environment(\.dismiss) var dismiss
     @State var date: Date = Date()
     @EnvironmentObject var diaryVM: DiaryViewModel
@@ -24,12 +26,21 @@ struct AddProgressLogView: View {
             VStack {
                 Heading(text: "Add progress log")
                 
+                // input fields for progress log
                 InputField(data: $weight, title: "Weight (kg)")
+                    .accessibilityLabel("Weight input field")
                 
                 InputField(data: $bodyFatPercentage, title: "Body fat percentage (%)")
+                    .accessibilityLabel("Body fat percentage input field")
+
                 InputField(data: $armMeasurement, title: "Arm measurement (cm)")
+                    .accessibilityLabel("Arm measurement input field")
+
                 InputField(data: $waistMeasurement, title: "Waist measurement (cm)")
+                    .accessibilityLabel("Waist measurement input field")
+
                 InputField(data: $chestMeasurement, title: "Chest measurement (cm)")
+                    .accessibilityLabel("Chest measuremen input field")
 
                 Spacer()
                 Button {
@@ -43,22 +54,28 @@ struct AddProgressLogView: View {
                 }
                 .buttonStyle(CTAButton())
                 .padding()
+                .accessibilityLabel("Add progress log button")
             }
         }
     }
     
+    /// function to handle adding progress log
     func handleAddProgressLog() {
         
+        // format date to string
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
 
         let dateString = dateFormatter.string(from: date)
         
+        // add data to the struct for API to handle
         var dataToAPI = ProgressDiaryEntryToAPI(date: dateString, weight: Double(weight) ?? 0.0, body_fat_percentage: Double(bodyFatPercentage), arm_measurement: Double(armMeasurement), waist_measurement: Double(waistMeasurement), chest_measurement: Double(chestMeasurement))
         
+        // call the method to send data over
         diaryVM.addToProgressDiary(progressDiaryEntry: dataToAPI) { result in
             switch result {
             case .success(_):
+                // show notification
                 notification = InAppNotification(style: .success, message: "Progress log added successfully")
             case .failure(let error):
                 notification = InAppNotification(style: .networkError, message: "Something went wrong, try again later")
@@ -68,7 +85,3 @@ struct AddProgressLogView: View {
         
     }
 }
-
-//#Preview {
-//    AddProgressLogView()
-//}
